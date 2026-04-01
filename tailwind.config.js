d1e60b112cdd9bcfaace605be8779c89bdeb6ea9
Md Khaled Saifullah <mks.saifullah@gmail.com>
2026-04-01 20:11:53 +0600

    our services

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,22 @@ module.exports = {
   plugins: [],
 }
 
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["*"],
+//   theme: {
+//     screens:{
+//     res3: "321px",
+//     res4: "481px",
+//     res5: "576px",
+//     res7: "768px",
+//     res9: "992px",
+//     res12: "1200px",
+//     res14: "1400px"
+//     },
+//     extend: {},
+//   },
+//   plugins: [],
+// }
